26cdd8a6307240bdf40495e728d8ccfec161841f
VainerAriel <arielvainer.main@gmail.com>
2024-05-11 23:10:53 -0400

    clean up code

--- Tile.py
+++ Tile.py
@@ -1,11 +1,11 @@
-import math
-
 from settings import *
 
 
+# floor, wall, water, signal
+
 class Tile:
     def __init__(self, x, y, w, h, color, fill=True, collider=False, pushable=False, sprites=None, frame_limit=2,
-                 timer_limit=300, hit_box=(5 * scale, 26 * scale), group="", transmit=False):
+                 timer_limit=300, hit_box=(5 * scale, 26 * scale), group="", transmit=False, tile_type="floor"):
         self.grid_pos = pygame.math.Vector2(x, y)
         self.pos = pygame.math.Vector2(x * tile_size, y * tile_size)
         self.size = pygame.math.Vector2(w, h)
@@ -24,37 +24,42 @@ class Tile:
         self.hit_box = hit_box
         self.group = group
         self.transmit = transmit
+        self.signal_timer = 0
 
-    def show(self):
-        pygame.draw.rect(screen, self.color, self.rect, 1 if self.fill else 0)
+        self.tile_type = tile_type
+
+    def show(self, time=0):
+        if self.tile_type != "signal":
+            pygame.draw.rect(screen, self.color, self.rect, 1 if self.fill else 0)
+        else:
+            print(self.sprites, "aaa")
+            screen.blit(self.sprites[self.frame], (self.rect.x, self.rect.y))
+        self.update_anim(time)
 
     def update_pos(self):
         self.rect = pygame.Rect(self.pos.x, self.pos.y, self.size.x, self.size.y)
-        # self.update_grid_pos()
 
     def update_grid_pos(self):
-        if self.pushable:
-            x, y = self.grid_pos
-            min_dist = 1000
-            new_x, new_y = x, y
-            for i in range(-1, 2):
-                for j in range(-1, 2):
-                    middle = ((x + j + 0.5) * tile_size, (y + i + 0.5) * tile_size)
-                    tile_middle = (self.pos.x + self.size.x / 2, self.pos.y + self.size.y / 2)
-                    # pygame.draw.line(screen, (0, 0, 255), middle, tile_middle)
-                    distance = math.dist(middle, tile_middle)
-                    if distance < min_dist:
-                        min_dist = distance
-                        new_x, new_y = x + j, y + i
+        pass
+        # if self.pushable:
+        #     x, y = self.grid_pos
+        #     min_dist = 1000
+        #     new_x, new_y = x, y
+        #     for i in range(-1, 2):
+        #         for j in range(-1, 2):
+        #             middle = ((x + j + 0.5) * tile_size, (y + i + 0.5) * tile_size)
+        #             tile_middle = (self.pos.x + self.size.x / 2, self.pos.y + self.size.y / 2)
+        #             # pygame.draw.line(screen, (0, 0, 255), middle, tile_middle)
+        #             distance = math.dist(middle, tile_middle)
+        #             if distance < min_dist:
+        #                 min_dist = distance
+        #                 new_x, new_y = x + j, y + i
 
-            # pygame.draw.line(screen, (0, 255, 255), ((new_x + 0.5) * tile_size, (new_y + 0.5) * tile_size),
-            #                  (self.pos.x + self.size.x / 2, self.pos.y + self.size.y / 2), 10)
+        # pygame.draw.line(screen, (0, 255, 255), ((new_x + 0.5) * tile_size, (new_y + 0.5) * tile_size),
+        #                  (self.pos.x + self.size.x / 2, self.pos.y + self.size.y / 2), 10)
 
     def update_anim(self, time):
         self.timer += time
         if self.timer > self.timer_limit:
             self.frame = (self.frame + 1) % (self.frame_limit if not self.transmit else 3)
             self.timer = 0
-
-    def collide(self, other):
-        return self.collider and other.collider and self.rect.colliderect(other.rect)
--- main.py
+++ main.py
@@ -1,149 +1,165 @@
 import sys
 
-import pygame
-
+from Box import Box
 from Player import Player
 from Tile import Tile
-from Wall import Wall
-from Water import Water
-from Box import Box
-from Signal import Signal
 from settings import *
 
 
 def load_level(level):
     tiles = []
+    boxes = []
     player = None
     for y in range(len(level)):
         for x in range(len(level[y])):
             tile_type = level[y][x]
             if tile_type == 0:
-                tiles.append(Tile(x, y, tile_size, tile_size, (0, 0, 0)))
-            elif tile_type == 2:
-                tiles.append(Tile(x, y, tile_size, tile_size, (0, 0, 0)))
-                player = Player(x, y, tile_size/2+2*scale, 3*tile_size/4-3*scale, (200, 50, 50))
+                tile = Tile(x, y, tile_size, tile_size, (0, 0, 0))
+                tiles.append(tile)
+
             elif tile_type == 1:
-                tiles.append(Wall(x, y, tile_size, tile_size, (150, 25, 150)))
-            elif tile_type == 4:
-                tiles.append(Water(x, y, tile_size, tile_size, (25, 25, 150)))
+                tile = Tile(x, y, tile_size, tile_size, (150, 25, 150), tile_type="wall", fill=False, collider=True)
+                tiles.append(tile)
+
+            elif tile_type == 2:
+                tile = Tile(x, y, tile_size, tile_size, (0, 0, 0))
+                tiles.append(tile)
+                player = Player(x, y, tile_size / 2 + 2 * scale, 3 * tile_size / 4 - 3 * scale, (200, 50, 50))
+
             elif tile_type == 3:
-                tiles.append(Tile(x, y, tile_size, tile_size, (0, 0, 0)))
-                tiles.append(Box(x, y, tile_size, tile_size, (50, 150, 50)))
-            elif tile_type == 5:
-                tiles.append(Tile(x, y, tile_size, tile_size, (0, 0, 0)))
-                tiles.append(Box(x, y, tile_size, tile_size, (50, 150, 50), group="g1", sprite=boulder))
-            elif tile_type == 6:
-                tiles.append(Tile(x, y, tile_size, tile_size, (0, 0, 0)))
-                tiles.append(Box(x, y, tile_size, tile_size, (50, 150, 50), group="g2", sprite=boulder))
+                tile = Tile(x, y, tile_size, tile_size, (0, 0, 0))
+                box = Box(x, y, tile_size, tile_size, (50, 150, 50), tile_type="box", group="leader")
+                tiles.append(tile)
+                boxes.append(box)
+
+            elif tile_type == 4:
+                tile = Tile(x, y, tile_size, tile_size, (25, 25, 150), tile_type="water", fill=False, collider=True)
+                tiles.append(tile)
+
+            elif tile_type in [5, 6]:
+                tile = Tile(x, y, tile_size, tile_size, (0, 0, 0))
+                box = Box(x, y, tile_size, tile_size, (50, 150, 50), tile_type="box", group=box_groups[tile_type], sprite=boulder[0])
+                tiles.append(tile)
+                boxes.append(box)
+
             elif tile_type == 7:
-                tiles.append(Signal(x, y, tile_size, tile_size, (50, 0, 50), signal))
-    return player, tiles
+                tile = Tile(x, y, tile_size, tile_size, (150, 25, 150), tile_type="signal", fill=False, sprites=signal,
+                            frame_limit=4, timer_limit=500)
+                tiles.append(tile)
+
+    return tiles, boxes, player
 
 
-def draw_main(player, tiles, direction, moving, frame):
-    screen.blit(level0_img, (0, 0))
-
-    # screen.blit(ground, (0, 0))
-    # screen.blit(water[frame], (0, 0))
-    # screen.blit(balloon, (0, 0))
-    # screen.fill((255, 255, 255))
-    # for tile in tiles:
-    #     if type(tile) == Tile:
-    #         tile.show()
-    # for tile in tiles:
-    #     if type(tile) == Wall:
-    #         tile.show()
-    # for tile in tiles:
-    #     if type(tile) == Water:
-    #         tile.show()
-
-    for tile in tiles:
-        if type(tile) == Box:
-            if tile.leader:
+def draw_main(tiles, boxes, player, frame, current_play, shapes=False):
+    if not shapes:
+        background_images = level_backgrounds[current_play]
+        for img in background_images:
+            if type(img) == list:
+                screen.blit(img[frame], (0, 0))
+            else:
+                screen.blit(img, (0, 0))
+    else:
+        screen.fill((255, 255, 255))
+        for tile in tiles:
+            if tile.tile_type == "floor":
                 tile.show()
+        for tile in tiles:
+            if tile.tile_type in ["wall", "water"]:
+                tile.show()
+
     for tile in tiles:
-        if type(tile) == Signal:
+        if tile.tile_type == "signal":
             tile.show(clock.get_time())
-            # pygame.draw.circle(screen, (150, 0, 150), (tile.rect.x+tile.rect.width/2, tile.rect.y+3*tile.rect.height/2), 2*tile_size/5)
-            print(tile.recharge, tile.signal_timer)
-    player.show(direction, moving)
-    # pygame.draw.rect(screen, (200, 0, 0), player.rect)
+    for box in boxes:
+        if box.leader:
+            box.show()
+    player.show()
+
+
+def player_movement(player, keys):
+    player.moving = 1
+    if player.control:
+        if keys[pygame.K_w]:
+            player.set_dir(player.vel.x, -1)
+            player.direction = 1
+            player.moving = 0
+        elif keys[pygame.K_s]:
+            player.set_dir(player.vel.x, 1)
+            player.direction = 3
+            player.moving = 0
+        else:
+            player.set_dir(player.vel.x, 0)
+
+        if keys[pygame.K_a]:
+            player.set_dir(-1, player.vel.y)
+            player.direction = 0 if player.vel.y == 0 else player.direction
+            player.moving = 0
+        elif keys[pygame.K_d]:
+            player.set_dir(1, player.vel.y)
+            player.direction = 2 if player.vel.y == 0 else player.direction
+            player.moving = 0
+        else:
+            player.set_dir(0, player.vel.y)
+    else:
+        player.moving = 0
 
 
 def main():
-    player, tiles = load_level(level1)
+    current_level = 0
+    current_level_select = 0
+    current_mode = "play"
+    shapes = current_level == 0 and current_mode == "play"
 
-    direction, moving = 0, 0
-    water_timer = 0
-    water_frame = 0
+    currently_displayed = level_select[current_level_select] if current_mode == "select" else levels[current_level]
+    current_play = current_mode + " " + str(current_level_select if current_mode == "select" else current_level)
+    tiles, boxes, player = load_level(currently_displayed)
 
-    boxes = []
-    for tile in tiles:
-        if tile.group == "g1":
-            boxes.append(tile)
-    minim = 100, 100, None
+    water_timer, water_frame = 0, 0
+
     for box in boxes:
-        if box.grid_pos.x < minim[0] or box.grid_pos.y < minim[1]:
-            minim = box.grid_pos.x, box.grid_pos.y, box
-    minim[2].leader = True
-    fade(screen, "in", draw_main, player, tiles, direction, moving, water_frame)
+        if box.group == "leader":
+            box_directions = [(1, 0), (0, 1)]
+            for d in box_directions:
+                tile = currently_displayed[int(box.grid_pos.y + d[0])][int(box.grid_pos.x + d[1])]
+
+                if tile in list(box_groups.keys()):
+                    tile_group = box_groups[tile]
+                    box.group = tile_group
+                    box.leader = True
+
+    fade(screen, "in", draw_main, tiles, boxes, player, water_frame, current_play, shapes)
 
     run = True
     while run:
         # dt = time.time() - last_frame
         # dt *= 30
         # last_frame = time.time()
-
         water_timer += clock.get_time()
 
         keys = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                fade(screen, "out", draw_main, player, tiles, direction, moving, water_frame)
+                fade(screen, "out", draw_main, tiles, boxes, player, water_frame, current_play, shapes)
                 run = False
 
-        moving = 1
-        if player.control:
-            if keys[pygame.K_w]:
-                player.set_dir(player.vel.x, -1)
-                direction = 1
-                moving = 0
-            elif keys[pygame.K_s]:
-                player.set_dir(player.vel.x, 1)
-                direction = 3
-                moving = 0
-            else:
-                player.set_dir(player.vel.x, 0)
-
-            if keys[pygame.K_a]:
-                player.set_dir(-1, player.vel.y)
-                direction = 0 if player.vel.y == 0 else direction
-                moving = 0
-            elif keys[pygame.K_d]:
-                player.set_dir(1, player.vel.y)
-                direction = 2 if player.vel.y == 0 else direction
-                moving = 0
-            else:
-                player.set_dir(0, player.vel.y)
-        else:
-            moving = 0
-
-        if run:
-            draw_main(player, tiles, direction, moving, water_frame)
+        player_movement(player, keys)
 
         for tile in tiles:
-            if type(tile) != Box:
+            if tile.tile_type != "box":
                 tile.update_pos()
-            else:
-                tile.update_pos(tiles)
             tile.update_anim(clock.get_time())
 
-        player.update(tiles, level1, clock.get_time())
+        for box in boxes:
+            box.update_pos(tiles)
 
+        player.update(tiles+boxes, clock.get_time())
 
         if water_timer > 900:
             water_timer = 0
             water_frame = (water_frame + 1) % 2
+
+        if run:
+            draw_main(tiles, boxes, player, water_frame, current_play, shapes)
 
         pygame.display.update()
         clock.tick(fps)
